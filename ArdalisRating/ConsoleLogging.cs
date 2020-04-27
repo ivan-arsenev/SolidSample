d121a974e2d4fb7047d175c82ad22dd29c9abc58
Ivan Arsenev <ivan_arsenev@pm.me>
2020-04-27 20:59:59 +0300

    Refactoring

---
+++ ArdalisRating/ConsoleLogging.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArdalisRating
+{
+    public class ConsoleLogging
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ ArdalisRating/JsonPolicySerializer.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArdalisRating
+{
+   public class JsonPolicySerializer
+    {
+
+        public Policy GetPolicyFromJson(string policyJson)
+        {
+           return JsonConvert.DeserializeObject<Policy>(policyJson, new StringEnumConverter());
+        }
+      
+    }
+}
