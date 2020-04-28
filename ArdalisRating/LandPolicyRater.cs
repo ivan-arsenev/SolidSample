9b81d98e2b9e0b830bbc577a6cf91596f0598ff7
ivan-arsenev <ivan_arsenev@pm.me>
2020-04-28 23:15:14 +0300

    Added OCP example

---
+++ ArdalisRating/LandPolicyRater.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArdalisRating
+{
+    public class LandPolicyRater
+    {
+        private readonly RatingEngine _ratingEngine;
+        private ConsoleLogger _loger;
+
+        public LandPolicyRater(RatingEngine engine, ConsoleLogger loger)
+        {
+            _ratingEngine = engine;
+            _loger = loger;
+        }
+
+        public void Rate(Policy policy)
+        {
+
+            _loger.Log("Rating LAND policy...");
+            _loger.Log("Validating policy.");
+            if (policy.BondAmount == 0 || policy.Valuation == 0)
+            {
+                _loger.Log("Land policy must specify Bond Amount and Valuation.");
+                return;
+            }
+            if (policy.BondAmount < 0.8m * policy.Valuation)
+            {
+                _loger.Log("Insufficient bond amount.");
+                return;
+            }
+            _ratingEngine.Rating = policy.BondAmount * 0.05m;
+            
+
+        }
+    }
+}
